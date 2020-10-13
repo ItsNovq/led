0ae74e6d6dc6604517da2301cf6462488d8b3e22
ItsNovq <71662894+ItsNovq@users.noreply.github.com>
2020-10-13 13:37:28 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,14 @@
+for (let index = 0; index < 40; index++) {
+    pins.digitalWritePin(DigitalPin.P0, 1)
+    basic.pause(100)
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    basic.pause(100)
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    basic.pause(100)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    basic.pause(100)
+    pins.digitalWritePin(DigitalPin.P2, 1)
+    basic.pause(100)
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    basic.pause(100)
+}
